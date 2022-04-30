0d37cfe8a6b7331d12674cf1b8a47b8d4d8aae63
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-30 16:07:37 -0500

    Implemented Merge sort.

--- CPlusPlus_Algorithms/MergeSort.h
+++ CPlusPlus_Algorithms/MergeSort.h
@@ -11,5 +11,70 @@ one sublist remaining. This final sublist is the sorted list.
 
 namespace MergeSort
 {
-	
+	void Sort( int unsortedArray[], int initialIndex, int finalIndex )
+	{
+		if( initialIndex >= finalIndex )
+			return;
+
+		int middleIndex = ( initialIndex + finalIndex ) / 2;
+		Sort( unsortedArray, initialIndex, finalIndex );
+		Sort( unsortedArray, middleIndex + 1, finalIndex );
+		Merge( unsortedArray, initialIndex, middleIndex, finalIndex );
+	}
+
+	void Merge( int unsortedArray[], int initialIndex, int middleIndex, int finalIndex )
+	{
+		const int firstArraySize = middleIndex - initialIndex + 1;
+		const int secondArraySize = finalIndex - middleIndex;
+
+		int* firstArray = new int[ firstArraySize ];
+		int* secondArray = new int[ secondArraySize ];
+
+		CopyArray( unsortedArray, firstArray, firstArraySize, initialIndex );
+		CopyArray( unsortedArray, secondArray, secondArraySize, middleIndex + 1 );
+
+		int firstArrayIndex = 0;
+		int secondArrayIndex = 0;
+		int mergedArrayIndex = initialIndex;
+
+		while( firstArrayIndex < firstArraySize && secondArrayIndex < secondArraySize )
+		{
+			if( firstArray[ firstArrayIndex ] < secondArray[ secondArrayIndex ] )
+			{
+				unsortedArray[ mergedArrayIndex ] = firstArray[ firstArrayIndex ];
+				firstArrayIndex++;
+			}
+			else
+			{
+				unsortedArray[ mergedArrayIndex ] = secondArray[ secondArrayIndex ];
+				secondArrayIndex++;
+			}
+			mergedArrayIndex++;
+		}
+
+		while( firstArrayIndex < firstArraySize )
+		{
+			unsortedArray[ mergedArrayIndex ] = firstArray[ firstArrayIndex ];
+			firstArrayIndex++;
+			mergedArrayIndex++;
+		}
+
+		while( secondArrayIndex < secondArraySize )
+		{
+			unsortedArray[ mergedArrayIndex ] = secondArray[ secondArrayIndex ];
+			secondArrayIndex++;
+			mergedArrayIndex++;
+		}
+
+		delete [] firstArray;
+		delete [] secondArray;
+	}
+
+	void CopyArray( int originalArray[], int targetArray[], int targetArraySize, int initialIndex )
+	{
+		for( auto index = 0; index < targetArraySize; index++ )
+		{
+			targetArray[ index ] = originalArray[ initialIndex + index ];
+		}
+	}
 }
