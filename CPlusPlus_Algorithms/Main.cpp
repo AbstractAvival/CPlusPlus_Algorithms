52ed12ebd4bf76879d594efbab52d3531f2bedbd
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-30 16:17:57 -0500

    Removed unused code.

--- CPlusPlus_Algorithms/Main.cpp
+++ CPlusPlus_Algorithms/Main.cpp
@@ -3,17 +3,6 @@
 
 int main()
 {
-	std::cout << "Hello C++ Algorithms!";
-
-	const int testSize = 10;
-	int test[ testSize ] = { 432, 765, 28, 999, 64, 651, 123, 88, 543, 0 };
-
-	MergeSort::Sort( test, 0, testSize - 1 );
-
-	for( auto number : test )
-	{
-		std::cout << number << std::endl;
-	}
-
+	std::cout << "Hello C++ Algorithms!" << std::endl;
 	return 0;
 }
