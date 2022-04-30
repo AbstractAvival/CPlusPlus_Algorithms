547d9996613cfe2f5c231a7badce94efa897757d
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-30 16:05:13 -0500

    Created MergeSort.h file. Added algorithm description.

---
+++ CPlusPlus_Algorithms/MergeSort.h
@@ -0,0 +1,15 @@
+/***************************************
+Merge Sort:
+
+An efficient, general-purpose, and coparison-based sorting
+algorithm. Merge sort is a divide and conquer algorithm. It
+divides the input array into halves until it has n sublists,
+then it repeatedly merges these sublists until there is only
+one sublist remaining. This final sublist is the sorted list. 
+***************************************/
+#pragma once
+
+namespace MergeSort
+{
+	
+}
