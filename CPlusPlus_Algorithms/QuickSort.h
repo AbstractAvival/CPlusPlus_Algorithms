0b5275ff64864710c13b5390faafd59d27fc24ab
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-04 13:29:59 -0500

    Created QuickSort file.

---
+++ CPlusPlus_Algorithms/QuickSort.h
@@ -0,0 +1,39 @@
+/***************************************
+Quick Sort:
+
+A divide and conquer algorithm. It picks an element as
+a pivot point and partitions the given array around said
+pivot into sub-arrays. The sub-arrays are then sorted 
+recursively.
+***************************************/
+#pragma once
+
+namespace QuickSort
+{
+	void Sort( int unsortedArray[], int startIndex, int endIndex )
+	{
+		if( startIndex >= endIndex )
+			return;
+
+		int pivotIndex = Partition( unsortedArray, startIndex, endIndex );
+		Sort( unsortedArray, startIndex, pivotIndex - 1 );
+		Sort( unsortedArray, pivotIndex + 1, endIndex );
+	}
+
+	int Partition( int unsortedArray[], int startIndex, int endIndex )
+	{
+		int pivotValue = unsortedArray[ endIndex ];
+		int currentPivotIndex = startIndex - 1;
+
+		for( int currentIndex = startIndex; currentIndex < endIndex; currentIndex++ )
+		{
+			if( unsortedArray[ currentIndex ] < pivotValue )
+			{
+				currentPivotIndex++;
+
+			}
+		}
+
+		return currentPivotIndex;
+	}
+}
