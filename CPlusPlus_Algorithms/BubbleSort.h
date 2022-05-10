0e216cedf21d8539d1a6118fdf30f882e260e402
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-10 13:45:06 -0500

    fixed typo

--- CPlusPlus_Algorithms/BubbleSort.h
+++ CPlusPlus_Algorithms/BubbleSort.h
@@ -2,7 +2,7 @@
 Bubble Sort:
 
 A sorting algorithm that compares two adjacent elements and
-swaps themif they are in the wrong order. This is repeated
+swaps them if they are in the wrong order. This is repeated
 until the list is sorted. Bubble sort is not a practical
 sorting algorithm.
 ***************************************/
